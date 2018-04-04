41c96ed5ba290c8e9de6a4ad28d1a6f5d721aa98
MJ Phillip <mj@resafrica.com>
2018-04-04 09:06:18 +0200

    Added guideline code

---
+++ script.js
@@ -0,0 +1,30 @@
+/*LEVEL 1
+---------*/
+//Create musician object
+
+
+//Add style of music to object
+
+
+//Console.log the name of musician and their style of music
+
+
+//Create an array of their album titles
+
+
+//Using a for loop console.log each album title
+
+
+//Check if musician is alive and log the correct statement.
+
+
+/*LEVEL 2
+---------*/
+//Create a class for a single of an album
+
+
+//Create to singles using the class
+
+
+//Console.log out the objects
+
