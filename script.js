e270586aacf02b6198cc37e7c6131d19e2eab13c
Maren Grimstad <36054558+maren793@users.noreply.github.com>
2018-04-22 22:08:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 /*LEVEL 1
 ---------*/
 //Create musician object
-
+var musician = {
+  name: "Ed Sheeran",
+  age: 27,
+  alive: true
+}
 
 //Add style of music to object
-
+musician.style = "Indiepop and Folk"
 
 //Console.log the name of musician and their style of music
-
+console.log(musician.name, musician.style);
 
 //Create an array of their album titles
-
+var array = ["+", "x Deluxe Edition)", "÷ (Deluxe")];
 
 //Using a for loop console.log each album title
-
+for(var i = 0; i < array.length; i++)
+  console.log(array[i]);
 
 //Check if musician is alive and log the correct statement.
 
