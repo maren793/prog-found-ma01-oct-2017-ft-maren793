db9f7c14a18f440b74cebfbaa2d01f66f3f56ea6
Maren Grimstad <36054558+maren793@users.noreply.github.com>
2018-04-22 23:13:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,22 +14,35 @@ musician.style = "Indiepop and Folk"
 console.log(musician.name, musician.style);
 
 //Create an array of their album titles
-var array = ["+", "x Deluxe Edition)", "÷ (Deluxe")];
+var array = ["+", "x Deluxe Edition)", "Divide")];
 
 //Using a for loop console.log each album title
 for(var i = 0; i < array.length; i++)
   console.log(array[i]);
 
 //Check if musician is alive and log the correct statement.
-
+if(musician.alive) {
+  console.log("Looking forward to hearing more");
+}
 
 /*LEVEL 2
 ---------*/
 //Create a class for a single of an album
-
+class Single {
+  constructor(name, released, length) {
+  this.name = name;
+  this.released = 2017;
+  this.length = 4:23;
+  }
+}
 
 //Create to singles using the class
+var single1 = new Single("Shape of you", "2017", "4:23");
+var single2 = new Single("Perfect", "2017", "4:40");
+
+var singles = [single1, single2];
 
 
 //Console.log out the objects
 
+console.log(singles);
